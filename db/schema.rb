97d387236dfd25f57936ea63e8242d39a24dc16b
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-21 17:29:06 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190421165958) do
+ActiveRecord::Schema.define(version: 20190421172636) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "passengers_taxis", id: false, force: :cascade do |t|
-    t.integer "taxi_id",      null: false
+  create_table "passengers_rides", id: false, force: :cascade do |t|
+    t.integer "ride_id",      null: false
     t.integer "passenger_id", null: false
   end
 
   create_table "rides", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "rides_taxis", id: false, force: :cascade do |t|
-    t.integer "ride_id", null: false
-    t.integer "taxi_id", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "passenger_id"
+    t.integer  "taxi_id"
   end
 
   create_table "taxis", force: :cascade do |t|
