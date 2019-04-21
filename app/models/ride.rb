789366fbf426f3f503ca417f56e8d4cc713023d4
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-21 17:14:52 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,8 +1,4 @@
 class Ride < ActiveRecord::Base
-  belongs_to :passengers
-  belongs_to :taxis
-  has_many :passengers 
-  has_many :taxis
-  has_many :rides_taxis 
-  has_many :passengers_rides 
+  has_many :taxis_rides 
+  has_many :passengers_rides
 end
